b7e741a34180664c1f742a1357ba8246fe895f5e
captaincommander@protonmail.com <captaincommander@protonmail.com>
2024-12-19 13:02:12 -0600

    fixed vite.config for production, adjusted deploy.yaml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/code-collab/',
+  base: process.env.NODE_ENV === 'production' ? '/code-collab/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
